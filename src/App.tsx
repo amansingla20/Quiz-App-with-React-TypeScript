22f7d4f4a1d239943cee8dceacd3973f1de1af37
AMAN SINGLA <amansingla222@gmail.com>
2022-02-14 19:37:48 +0530

    Components added, image added, some logics are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QuestionCard from './Components/QuestionCard';
+
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+
+
+  
+  const startQuiz = async() => {
+  }
+
+  const checkAnswer = (event : React.MouseEvent<HTMLButtonElement>) => { 
+
+   }
+
+   const nextQuestion = () =>{
+
+   }
+
   return (
     <div className="App">
-     <p>Hi Welcome Back...</p>
+     <h1> QUIZ</h1>
+     <button className ="start">
+       Start
+     </button>
+     <p className='score'>Score:</p>
+     <p>Loading Questions...</p>
+    <QuestionCard/>
+    <button className = "next" onClick = {nextQuestion}>
+      Next Question
+    </button>
     </div>
   );
 }
